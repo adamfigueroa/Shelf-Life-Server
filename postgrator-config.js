ff49d1c7ea706e563388f6404573e29e36fae460
Adam Figueroa <admiralfiggins@gmail.com>
2021-02-02 09:09:10 -0800

    server fixes

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
 
+const pg = require("pg");
+pg.defaults.ssl =
+  process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
+
 module.exports = {
   migrationsDirectory: "migrations",
   driver: "pg",
--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
-const app = require('./app');
-const knex = require('knex');
-const { PORT, DATABASE_URL } = require('./config');
+const app = require("./app");
+const knex = require("knex");
+const { PORT, DATABASE_URL } = require("./config");
+
+const pg = require("pg");
+pg.defaults.ssl =
+  process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: DATABASE_URL,
 });
 
-app.set('db', db);
+app.set("db", db);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
